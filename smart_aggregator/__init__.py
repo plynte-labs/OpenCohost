6aa5116c24963a1fc9df58645b47046a5c87cd0f
FranGuh <73907566+FranGuh@users.noreply.github.com>
2026-05-14 18:47:55 -0600

    feat: export KiraAgendaController classes from smart_aggregator package
    Expose AgendaAction, AgendaState, AgendaTopic, KiraAgendaController,
    and TopicStatus for use by app_shell and UI panels.

--- smart_aggregator/__init__.py
+++ smart_aggregator/__init__.py
@@ -1,3 +1,11 @@
 from .aggregator import Aggregator
+from .kira_agenda_controller import AgendaAction, AgendaState, AgendaTopic, KiraAgendaController, TopicStatus
 
-__all__ = ["Aggregator"]
+__all__ = [
+    "Aggregator",
+    "AgendaAction",
+    "AgendaState",
+    "AgendaTopic",
+    "KiraAgendaController",
+    "TopicStatus",
+]
